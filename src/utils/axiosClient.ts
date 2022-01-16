5f49be94810826d779512db7b7208d71a8005406
Nikolaos Papageorgiou <nikos19pap@gmail.com>
2022-01-16 22:37:36 +0200

    add https to endpoints

--- src/utils/axiosClient.ts
+++ src/utils/axiosClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosClient = axios.create();
 
-axiosClient.defaults.baseURL = 'http://demo7478931.mockable.io/';
+axiosClient.defaults.baseURL = 'https://demo7478931.mockable.io/';
 
 export function getRequest(URL: string) {
   return axiosClient
